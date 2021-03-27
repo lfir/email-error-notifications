9b87fd8b93430144bb15d7aedf02fca413314d20
Asta86 <psljp@protonmail.com>
2021-03-27 16:39:31 -0300

    Add reading environment variables test.

--- tests/test_mail_error_notifs.py
+++ tests/test_mail_error_notifs.py
@@ -1,16 +1,23 @@
 import io
+import os
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
+from dotenv import load_dotenv
 from mail_error_notifs.mail_error_notifs import SendGridAPIClient, send_email
 
 
 class TestScripts(unittest.TestCase):
 
+    def test_when_env_file_present_environment_variables_can_be_read(self):
+        load_dotenv('./.env')
+        self.assertIsNotNone(os.getenv('LOGDIR'))
+
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_when_email_sent_then_statuscode_printed_is_202(self, mock_stdout):
+    def test_when_email_sent_then_statuscode_printed_is_the_one_returned_by_sendgridclient(self, mock_stdout):
+        SendGridAPIClient.send = Mock()
         send_email('TestCase subject', 'TestCase body')
-        self.assertIn('status code: 202', mock_stdout.getvalue())
+        self.assertIn('SendGrid response status', mock_stdout.getvalue())
 
     def test_when_email_not_sent_then_error_is_logged(self):
         err_msg = 'Send mail error'
