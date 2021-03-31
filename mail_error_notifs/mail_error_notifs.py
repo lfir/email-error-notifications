d25efba5734556896a66a163ef1060c2b1d72021
Asta86 <psljp@protonmail.com>
2021-03-31 14:58:38 -0300

    Add docstring to function send_email.

--- mail_error_notifs/mail_error_notifs.py
+++ mail_error_notifs/mail_error_notifs.py
@@ -10,6 +10,13 @@ from sendgrid.helpers.mail import From, Mail, PlainTextContent, Subject, To
 
 
 def send_email(subject, mailbody):
+    """Sends an email using Sendgrid's service to one or more recipients.
+    FROM and TO addresses and SENDGRID_API_KEY need to be available as environment variables.
+
+    Positional arguments:
+    subject -- A string with the subject of the email.
+    mailbody -- A string with the body of the email. Content type will be text/plain.
+    """
     try:
         sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
         from_email = From(os.getenv('FROM'), 'Chronos Maybelambda')
@@ -17,12 +24,13 @@ def send_email(subject, mailbody):
         subject = Subject(subject)
         content = PlainTextContent(mailbody)
         mail = Mail(from_email, to_emails, subject, content)
-        
+
         response = sg.send(mail)
         print('SendGrid response status code:', response.status_code)
         print('SendGrid response body:', response.body)
     except Exception as e:
         logging.error(e)
+
 
 if __name__ == '__main__':
     load_dotenv('./.env')
