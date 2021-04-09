dc1121d53042b890cf65ddf571ce7d7021370b40
Asta86 <psljp@protonmail.com>
2021-04-09 00:09:05 -0300

    Black formatting.

--- mail_error_notifs/mail_error_notifs.py
+++ mail_error_notifs/mail_error_notifs.py
@@ -18,39 +18,39 @@ def send_email(subject, mailbody):
     mailbody -- A string with the body of the email. Content type will be text/plain.
     """
     try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
-        from_email = From(os.getenv('FROM'), 'Chronos Maybelambda')
-        to_emails = To(os.getenv('TO'))
+        sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
+        from_email = From(os.getenv("FROM"), "Chronos Maybelambda")
+        to_emails = To(os.getenv("TO"))
         subject = Subject(subject)
         content = PlainTextContent(mailbody)
         mail = Mail(from_email, to_emails, subject, content)
 
         response = sg.send(mail)
-        print('SendGrid response status code:', response.status_code)
-        print('SendGrid response body:', response.body)
+        print("SendGrid response status code:", response.status_code)
+        print("SendGrid response body:", response.body)
     except Exception as e:
         logging.error(e)
 
 
-if __name__ == '__main__':
-    load_dotenv('./.env')
+if __name__ == "__main__":
+    load_dotenv("./.env")
     logging.basicConfig(
-        filename=PurePath(os.getenv('LOGDIR')) / 'mail_error_notifs.log',
+        filename=PurePath(os.getenv("LOGDIR")) / "mail_error_notifs.log",
         level=logging.ERROR,
-        format='%(asctime)s|%(levelname)s|%(message)s'
+        format="%(asctime)s|%(levelname)s|%(message)s",
     )
     errors = set()
-    msg = 'URL: {}.\nStatus code: {}.\nMessage: {}.'
-    req0 = request.Request(os.getenv('HV1'))
-    headers = {os.getenv('H1'): req0.full_url}
-    req1 = request.Request(url=os.getenv('URL1'), headers=headers)
+    msg = "URL: {}.\nStatus code: {}.\nMessage: {}."
+    req0 = request.Request(os.getenv("HV1"))
+    headers = {os.getenv("H1"): req0.full_url}
+    req1 = request.Request(url=os.getenv("URL1"), headers=headers)
 
     for req in [req0, req1]:
         try:
             request.urlopen(req)
         except HTTPError as e:
-            logging.error(e.geturl() + ': ' + str(e))
+            logging.error(e.geturl() + ": " + str(e))
             errors.add(msg.format(req.full_url, e.code, e.read()))
 
     if len(errors) > 0:
-        send_email('Notification of site errors', '\n\n'.join(errors))
+        send_email("Notification of site errors", "\n\n".join(errors))
--- tests/test_mail_error_notifs.py
+++ tests/test_mail_error_notifs.py
@@ -8,25 +8,28 @@ from mail_error_notifs.mail_error_notifs import SendGridAPIClient, send_email
 
 
 class TestScripts(unittest.TestCase):
-
     def test_when_env_file_present_environment_variables_can_be_read(self):
-        load_dotenv('./.env')
-        self.assertIsNotNone(os.getenv('LOGDIR'))
+        load_dotenv("./.env")
+        self.assertIsNotNone(os.getenv("LOGDIR"))
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_when_email_sent_then_statuscode_printed_is_the_one_returned_by_sendgridclient(self, mock_stdout):
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_when_email_sent_then_statuscode_printed_is_the_one_returned_by_sendgridclient(
+        self, mock_stdout
+    ):
         SendGridAPIClient.send = Mock()
-        send_email('TestCase subject', 'TestCase body')
-        self.assertIn('SendGrid response status', mock_stdout.getvalue())
+        send_email("TestCase subject", "TestCase body")
+
+        self.assertIn("SendGrid response status", mock_stdout.getvalue())
 
     def test_when_email_not_sent_then_error_is_logged(self):
-        err_msg = 'Send mail error'
+        err_msg = "Send mail error"
 
         def mockinit(self, apikey):
             raise RuntimeError(err_msg)
-        
-        sendgridmock = patch.object(SendGridAPIClient, '__init__', mockinit)
+
+        sendgridmock = patch.object(SendGridAPIClient, "__init__", mockinit)
 
         with sendgridmock, self.assertLogs() as cm:
-            send_email('TestCase subject', 'TestCase body')
-            self.assertIn(err_msg, ' '.join(cm.output))
+            send_email("TestCase subject", "TestCase body")
+
+            self.assertIn(err_msg, " ".join(cm.output))
